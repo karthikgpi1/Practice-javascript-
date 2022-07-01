8cd4f2ebba30cb340ae2cb924a0d44287b8b4d72
karthikgpi1 <karthikgopi024@gmail.com>
2022-06-30 23:19:31 -0700

    Array_concat

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
-function kar() {
-    console.log("this function");
-}
+//--------------------
 
-kar();
 
+// let arr1 = [1,2,3,4,5];
+// let arr2 = [6,7,8,9,10];
+
+
+// console.log(arr1, arr2);
+
+// console.log(arr1.concat(arr2));    //es5
+
+// console.log([...arr1, ...arr2]);   // es6
+
+// console.log(...arr1, ...arr2);      //this is new method
+                                   //for using without cama
+
+//------------------------------------------------
+
+
+// let arr1 = [1,2,3,4,5];
+// let arr2 = [6,7,8,9,10];
+
+
+// let obj1 = {
+//     a: 1,
+//     b: 2,
+// };
+
+// let obj2 = {
+//     c:3,
+//     d: 4
+// };
+
+
+// console.log([...arr1, ...arr2]);
+
+// console.log({...obj1, ...obj2});
+
+//-----------------------------------------------
